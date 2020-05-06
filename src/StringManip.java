22cb672c8290ca61d22ddc367c557a4a64431f88
Lucy <thomalu@ssfss.org>
2020-05-06 12:37:54 -0400

    finished StringManip.

---
+++ src/StringManip.java
@@ -0,0 +1,41 @@
+public class StringManip
+{
+    /** Takes a string str and returns a new string * with all spaces removed.
+     */
+    public static String removeSpaces(String str)
+    {
+        String noSpace = "";
+        for (int i=0; i<str.length(); i++) {
+            if (!str.substring(i, i+1).equals(" ")) {
+                noSpace += str.substring(i, i+1);
+            }
+        }
+        return noSpace;
+    }
+
+
+    /** Takes a string str and returns a new string
+     * with the characters reversed, as described in part (a). */
+    public static String reverseString(String str)
+    {
+        String s = str.substring(str.length()-1);
+        for (int i = str.length()-1; i > 0; i--){
+            s += str.substring(i-1, i);
+        }
+        return s;
+    }
+
+
+    /** Determines whether str is a palindrome and prints a message
+     * indicating the result, as described in part (b).
+     * Precondition: str contains only lowercase letters and spaces.
+     */
+    public static void palindromeChecker(String str)
+    {
+        if (removeSpaces(str).equals(removeSpaces(reverseString(str))))
+            System.out.println("This is a palindrome!");
+        else
+            System.out.println("This is not a palindrome!");
+    }
+
+}
